70799a157d53759ea3f2348e061af510a5e5f488
Lenard Frommelt <lenny09918050@gmail.com>
2024-11-10 16:00:22 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,7 +1,14 @@
 module.exports = {
     getDependencies: () => {
         return {
-	    "thingy-sci-base":"^0.0.4"
+            // "cached-persistentstate": "^0.0.1",
+            // "key-block": "^0.0.2",
+            // "memory-decay": "^0.0.3",
+            // "secret-manager-crypto-utils": "^0.3.0",
+    	    // "thingy-sci-ws-base":"^0.0.1",
+            // "thingy-schema-validate": "^0.0.4",
+            // "thingy-session-utils": "^0.0.3",
+            // "validatabletimestamp": "^0.0.1"
         }
     }
 }
